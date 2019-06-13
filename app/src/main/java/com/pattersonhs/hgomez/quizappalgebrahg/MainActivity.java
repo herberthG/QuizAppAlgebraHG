91432dd2d693682e071006222b5022c09c22cb4a
herberthG <perezenrique362@gmail.com>
2019-06-13 09:58:59 -0400

    add quetion and true or false

--- app/src/main/java/com/pattersonhs/hgomez/quizappalgebrahg/MainActivity.java
+++ app/src/main/java/com/pattersonhs/hgomez/quizappalgebrahg/MainActivity.java
@@ -1,10 +1,12 @@
 package com.pattersonhs.hgomez.quizappalgebrahg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView greeting;
     Button submitButton;
     Button SubmitButton2;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,36 @@ public class MainActivity extends AppCompatActivity {
         greeting = (TextView) findViewById(R.id.greeting);
         submitButton = (Button) findViewById(R.id.submitButton);
         SubmitButton2 = (Button) findViewById(R.id.submitButton2);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = "You got it wrong";
+                Toast popUp = Toast.makeText( MainActivity.this,message, Toast.LENGTH_LONG);
+                popUp.show();
+                if(score > 0)
+
+                {
+                    score --;
+                }
+                Intent intent = new Intent( MainActivity.this, ScoreActivity.class);
+                intent.putExtra( "score",score);
+                startActivity(intent);
+
+
+
+            }
+        });
+
+        SubmitButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message2 = "Good Job! you anwers is right";
+                Toast popUp = Toast.makeText( MainActivity.this, message2, Toast.LENGTH_LONG);
+                popUp.show();
+                score++;
+            }
+        });
 
 
     }
